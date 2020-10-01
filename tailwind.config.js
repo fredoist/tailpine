6b55dad9e7555c72989c32f50e4e8a2d205a09e2
Freddy Gonzalez <7684330+agonzlezr@users.noreply.github.com>
2020-10-01 15:20:49 -0500

    Added tailwind config with build and purge options

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  future: {
+    // removeDeprecatedGapUtilities: true,
+    // purgeLayersByDefault: true,
+  },
+  purge: ["./**/*.html"],
+  theme: {
+    extend: {},
+  },
+  variants: {},
+  plugins: [],
+}

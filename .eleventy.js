da0e523c6550ef45b68b734918ee375d3531f21e
Freddy González <7684330+fredoist@users.noreply.github.com>
2022-04-20 11:28:45 -0500

    refactor: move styles and js out of assets dir

--- .eleventy.js
+++ .eleventy.js
@@ -2,8 +2,7 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.setQuietMode(true)
   eleventyConfig.addPassthroughCopy({
     public: '.',
-    'public/img': 'assets/img',
-    'node_modules/alpinejs/dist/cdn.min.js': 'assets/js/alpine.js'
+    'node_modules/alpinejs/dist/cdn.min.js': '/alpine.js'
   })
   eleventyConfig.addShortcode('version', function () {
     return Date.now()
